02b62435ab5d82c320ba2e26ea43135912f96f41
Godwhitelight <73111474+Godwhitelight@users.noreply.github.com>
2021-06-21 14:55:02 +0300

    Add files via upload

---
+++ FontUtil.java
@@ -0,0 +1,20 @@
+package God.Shush.Fonts;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
+
+import java.awt.*;
+
+public class FontUtil {
+
+    public static Font getFontFromTTF(ResourceLocation fontLocation, float fontSize, int fontType) {
+        Font output = null;
+        try {
+            output = Font.createFont(fontType, Minecraft.getMinecraft().getResourceManager().getResource(fontLocation).getInputStream());
+            output = output.deriveFont(fontSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+}
